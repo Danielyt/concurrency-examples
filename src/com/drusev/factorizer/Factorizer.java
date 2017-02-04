c7c65617df467a4614064be95bec03f492cf6037
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-02-04 20:22:38 +0200

    implement algorith for integer factorization

--- src/com/drusev/factorizer/Factorizer.java
+++ src/com/drusev/factorizer/Factorizer.java
@@ -3,10 +3,34 @@
  */
 package com.drusev.factorizer;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Drusev
  *
  */
 public class Factorizer {
+	
+	public List<BigInteger> factorize (final BigInteger number) {
+		return factor(number);
+	}
+	
+	private List<BigInteger> factor(final BigInteger number) {
+		int n = number.intValue();
+		List<BigInteger> factorList = new ArrayList<>();
+		for (int i = 2; i <= n / i; i++) {
+			while (n % i == 0) {
+				factorList.add(BigInteger.valueOf(i));
+				n /= i;				
+			}
+		}
+		
+		if (n > 1) {
+			factorList.add(BigInteger.valueOf(n));
+		}
+		return factorList;
+	}
 
 }
