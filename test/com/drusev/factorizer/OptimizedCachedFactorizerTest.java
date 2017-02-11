a322b87945b91233753a70ddefe0fd758f1310a4
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-02-11 14:36:08 +0200

    write unit tests for OptimizedCachedFactorizer.java

---
+++ test/com/drusev/factorizer/OptimizedCachedFactorizerTest.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.drusev.factorizer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.google.testing.threadtester.AnnotatedTestRunner;
+import com.google.testing.threadtester.ThreadedAfter;
+import com.google.testing.threadtester.ThreadedBefore;
+import com.google.testing.threadtester.ThreadedMain;
+import com.google.testing.threadtester.ThreadedSecondary;
+
+/**
+ * @author Drusev
+ *
+ */
+public class OptimizedCachedFactorizerTest {
+
+	private OptimizedCachedFactorizer factorizer;
+
+	private final BigInteger number = BigInteger.valueOf(45);
+
+	private final List<BigInteger> expectedFactors = Arrays.asList(BigInteger.valueOf(3), BigInteger.valueOf(3),
+			BigInteger.valueOf(5));
+
+	private List<BigInteger> actualFactorsFromMain;
+
+	private List<BigInteger> actualFactorsFromSecondary;
+
+	@ThreadedBefore
+	public void before() {
+		factorizer = new OptimizedCachedFactorizer();
+	}
+
+	@ThreadedMain
+	public void mainThread() {
+		actualFactorsFromMain = factorizer.factorize(number);
+	}
+
+	@ThreadedSecondary
+	public void secondaryThread() {
+		actualFactorsFromSecondary = factorizer.factorize(number);
+	}
+
+	@ThreadedAfter
+	public void after() {
+		assertEquals(expectedFactors, actualFactorsFromMain);
+		assertEquals(expectedFactors, actualFactorsFromSecondary);
+	}
+
+	@Test
+	public void testCaching() {
+		AnnotatedTestRunner runner = new AnnotatedTestRunner();
+		runner.runTests(this.getClass(), OptimizedCachedFactorizer.class);
+	}
+
+	@Test
+	public void testPerformance() {
+		CachedFactorizerPerformanceTest.testPerformance(new OptimizedCachedFactorizer(), 10000000, 20, 1000000,
+				10000000);
+	}
+}
