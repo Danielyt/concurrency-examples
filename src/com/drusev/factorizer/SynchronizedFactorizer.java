11cd35b7c292f1af37b2664f88662f403e656153
Daniel Rusev <danielytt_rusev@yahoo.com>
2017-02-11 10:41:57 +0200

    create class SynchronizedFactorizer.java

---
+++ src/com/drusev/factorizer/SynchronizedFactorizer.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.drusev.factorizer;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author Drusev
+ *
+ */
+public class SynchronizedFactorizer extends Factorizer {
+
+	private final AtomicReference<BigInteger> lastNumber = new AtomicReference<>();
+
+	private final AtomicReference<List<BigInteger>> lastFactors = new AtomicReference<>();
+
+	@Override
+	public synchronized List<BigInteger> factorize(final BigInteger number) {
+		if (number.equals(lastNumber.get())) {
+			return lastFactors.get();
+		} else {
+			List<BigInteger> factors = factor(number);
+			lastNumber.set(number);
+			lastFactors.set(factors);
+			return factors;
+		}
+	}
+}
